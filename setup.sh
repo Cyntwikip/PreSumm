993c3066b68b02597c1d15c493b3ce7702e3a7e3
Jude Teves <cyntwikip@gmail.com>
2020-04-27 14:23:41 +0800

    Polish code

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+chmod +x src/eva_train.sh
+chmod +x src/eva_test.sh
+chmod +x src/eva_forecast.sh
+
 wget http://nlp.stanford.edu/software/stanford-corenlp-full-2018-10-05.zip
 unzip stanford-corenlp-full-2018-10-05.zip
 export CLASSPATH=$(pwd)/stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2.jar
